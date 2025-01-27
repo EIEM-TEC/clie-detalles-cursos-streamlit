773557e7b416477df0e8a831ba0a0d5cf294f4a4
Juan J. Rojas <48027894+juanjorojash@users.noreply.github.com>
2025-01-26 21:18:25 -0600

    initial deploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,20 @@
 import streamlit as st
 import pandas as pd
-from openai import OpenAI
 
-client = OpenAI(
-  organization='org-UQuGzwzlmTQL61uA4vE5ZTkn',
-  project='proj_Qj5H908LWvkgwtSuRGYhAhY0',
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(
+    page_title='Detalles de cursos - CLIE - EIEM - TEC',
 )
 
-st.title("Generador de descripciones de cursos CLIE")
+
+'''
+# Detalles de cursos
+#### Comisión para la creación de la Licenciatura en Ingeniería Electromecánica
+Escuela de Ingeniería Electromecánica - Tecnológico de Costa Rica
+'''
 
 areas = pd.read_csv("https://raw.githubusercontent.com/EIEM-TEC/CLIE/refs/heads/main/areas.csv")
-cursos = pd.read_csv("https://raw.githubusercontent.com/EIEM-TEC/CLIE/refs/heads/main/cursos/cursos_malla.csv")
+cursosraw = pd.read_csv("https://raw.githubusercontent.com/EIEM-TEC/CLIE/refs/heads/main/cursos/cursos_malla.csv")
 rasgos = pd.read_csv("https://raw.githubusercontent.com/EIEM-TEC/CLIE/refs/heads/main/rasgos.csv")
 saberes = pd.read_csv("https://raw.githubusercontent.com/EIEM-TEC/CLIE/refs/heads/main/saberes.csv")
 cursos_rasgos = pd.read_csv("https://raw.githubusercontent.com/EIEM-TEC/CLIE/refs/heads/main/cursos/cursos_rasgos.csv")
@@ -20,17 +24,61 @@ nomArea = st.selectbox(
     areas[areas["nombre"] != "Total"]["nombre"])
 
 codArea = areas[areas["nombre"]==nomArea]["codArea"].item()
-cursos = cursos[(cursos["area"]==codArea) & (cursos["semestre"]<=10)]
+cursos = cursosraw[(cursosraw["area"]==codArea)\
+                   & (cursosraw["semestre"]<=10)\
+                   & (cursosraw["nombre"]!="Electiva I")\
+                   & (cursosraw["nombre"]!="Electiva II") ]
 saberes = saberes[saberes["codArea"]==codArea].copy()
-
 
 nomCurso = st.selectbox(
     "Curso",
     cursos[cursos["area"]==codArea]["nombre"])
 
+st.markdown("### Detalles:")
 
 codCurso = cursos[cursos["nombre"]==nomCurso]["codigo"].item()
+
 idCurso = cursos[cursos["nombre"]==nomCurso]["id"].item()
+
+semes = cursos[cursos["nombre"]==nomCurso]["semestre"].item()
+
+cred = cursos[cursos["nombre"]==nomCurso]["creditos"].item()
+
+hteor = cursos[cursos["nombre"]==nomCurso]["horasTeoria"].item()
+
+hprac = cursos[cursos["nombre"]==nomCurso]["horasPractica"].item()
+
+st.markdown(
+f'''
+* ID: {idCurso}
+* Código: {codCurso}
+* Semestre: {semes}
+* Creditos: {cred}
+* Horas: {hteor + hprac}
+* Horas teoría: {hteor}
+* Horas práctica: {hprac}
+* Horas extraclase: {(cred * 3)-(hteor + hprac)}
+''')
+
+st.markdown("*Requisitos:*")
+
+req = cursos[cursos["nombre"]==nomCurso]["requisitos"].str.split(';', expand=False).item()
+if str(req) != "nan":
+    for index in range(len(req)):
+        curReq = cursosraw[cursosraw["id"]==req[index]]["nombre"].item()
+        st.markdown(f"* {curReq}") 
+else:
+    st.markdown("* No")
+
+st.markdown("*Correquisitos:*")
+
+cor = cursos[cursos["nombre"]==nomCurso]["correquisitos"].str.split(';', expand=False).item()
+if str(cor) != "nan":
+    for index in range(len(cor)):
+        curCor = cursosraw[cursosraw["id"]==cor[index]]["nombre"].item()
+        st.markdown(f"* {curCor}") 
+else:
+    st.markdown("* No")
 
 codSaber = cursos_rasgos[cursos_rasgos["id"]==idCurso]["codSaber"].str.split(';', expand=False).item()
 
@@ -40,50 +88,14 @@ rasgos = rasgos.explode("codSaber") #expadir la lista
 
 codRasgos = rasgos[rasgos["codSaber"].isin(codSaber)]["rasgo"].unique()
 
-st.markdown("## Saberes del curso:")
+st.markdown("### Saberes:")
 
 for index in range(len(codSaber)):
     saber = saberes[saberes["codSaber"]==codSaber[index]]["nombre"].item()
     st.markdown(f"* {saber}")
 
-st.markdown("## Rasgos del curso:")
+st.markdown("### Rasgos:")
 
 for index in range(len(codRasgos)):
     rasgo = codRasgos[index]
     st.markdown(f"* {rasgo}")
-
-
-assistant = client.beta.assistants.create(
-    name="Asistente para creación de cursos",
-    instructions="Eres un experto en diseño instruccional y de curriculo. Responde siempre en español",
-    model="gpt-4o",
-    tools=[{"type": "file_search"}],
-)
-
-
-# if "openai_model" not in st.session_state:
-#     st.session_state["openai_model"] = "gpt-4o"
-
-# if "messages" not in st.session_state:
-#     st.session_state.messages = []
-
-# for message in st.session_state.messages:
-#     with st.chat_message(message["role"]):
-#         st.markdown(message["content"])
-
-# if prompt := st.chat_input("What is up?"):
-#     st.session_state.messages.append({"role": "user", "content": prompt})
-#     with st.chat_message("user"):
-#         st.markdown(prompt)
-
-#     with st.chat_message("assistant"):
-#         stream = client.chat.completions.create(
-#             model=st.session_state["openai_model"],
-#             messages=[
-#                 {"role": m["role"], "content": m["content"]}
-#                 for m in st.session_state.messages
-#             ],
-#             stream=True,
-#         )
-#         response = st.write_stream(stream)
-#     st.session_state.messages.append({"role": "assistant", "content": response})
